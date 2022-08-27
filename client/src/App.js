f83517ab0c8829ea192aa4548fd5f1b3688999d3
Disaeorous <105557505+Disaeorous@users.noreply.github.com>
2022-08-27 22:54:09 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from './styles/main.module.css';
 import { Stocks } from './components/Stocks';
 // import { Finance } from './pages/Finance';
@@ -8,14 +7,6 @@ export const App = () => {
     <div className={styles.container}>
       {/* <Finance /> */}
       <Stocks></Stocks>
-=======
-import { Finance } from './pages/Finance';
-
-export const App = () => {
-  return (
-    <div>
-      <Finance />
->>>>>>> 399e79fb0e7eff180c2643e890e6943f55a1013e
     </div>
   );
 };
